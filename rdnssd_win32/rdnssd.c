a3abc9748d10ccc089676ad1e2be6b8eefdc09b1
s-vincent <s-vincent@ea60ee8e-f36a-45ab-a58d-da2b63a087f7>
2014-02-08 18:03:39 +0000

    Fixes a bug where an entry is always added.

--- rdnssd_win32/rdnssd.c
+++ rdnssd_win32/rdnssd.c
@@ -350,13 +350,14 @@ static void rdnssd_update(struct in6_addr* addr, unsigned int ifindex, time_t ex
             if((expiry - servers.list[MAX_RDNSS - 1].expiry) >= 0)
                 i = MAX_RDNSS - 1;
         }
+
+      memcpy(&servers.list[i].addr, addr, sizeof(*addr));
+      servers.list[i].ifindex = ifindex;
+      servers.list[i].expiry = expiry;
+
+      qsort(servers.list, servers.count, sizeof(rdnss_t), rdnssd_is_older);
     }
 
-    memcpy(&servers.list[i].addr, addr, sizeof(*addr));
-    servers.list[i].ifindex = ifindex;
-    servers.list[i].expiry = expiry;
-
-    qsort(servers.list, servers.count, sizeof(rdnss_t), rdnssd_is_older);
     /*
     #ifndef NDEBUG
         for(unsigned i = 0; i < servers.count; i++)
